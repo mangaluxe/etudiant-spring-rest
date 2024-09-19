316449b282d13e5eb49894eccfe0be8bfac25d51
Mangaluxe <haiou@mangaluxe.com>
2024-09-19 12:28:44 +0200

    Ajout de @RequestMapping

--- src/main/java/org/example/etudiant/controller/UtilisateurController.java
+++ src/main/java/org/example/etudiant/controller/UtilisateurController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 
 @RestController
+@RequestMapping("api/utilisateur/")
 public class UtilisateurController {
 
     // ========== Propriétés ==========
@@ -31,7 +32,6 @@ public class UtilisateurController {
     // ========== Méthodes ==========
 
     @GetMapping
-    @RequestMapping("api/utilisateur/")
     public ResponseEntity<List<UtilisateurDtoSend>> getData() {
         List<Utilisateur> utilisateurs = utilisateurService.getAll();
         List<UtilisateurDtoSend> utilisateurDtoSends = new ArrayList<>();
